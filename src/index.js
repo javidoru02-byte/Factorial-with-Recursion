7988481b92807a422995d594b45a1b5df7725698
David <javidoru02@gmail.com>
2026-04-24 00:52:15 +0300

    fix: factorialWithRecursion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 function factorialWithRecursion(num) {
+  if (num !== 0 && !num) {
+    return "Помилка: хиьний тип данних";
+  }
+  if (!Number.isInteger(num)) {
+    return "Помилка:  значення має бути цілим";
+  }
+  if (num < 0) {
+    return "Помилка: факторіал не може бути відємного числа";
+  }
   if (num === 0) {
     return 1;
   }
   return num * factorialWithRecursion(num - 1);
 }
 
-const ANSWER = factorialWithRecursion(12);
+const ANSWER = factorialWithRecursion(-1);
 
 console.log(ANSWER);
